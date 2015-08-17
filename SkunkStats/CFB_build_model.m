bcbdcde0e97e3dd5ac607d3c6e4c40fc0726b266
Arthur <arajala@umich.edu>
2015-08-17 14:54:03 -0400

    updating training and testing functions, downloaded bowl scores, trying to download bowl week stats

--- SkunkStats/CFB_build_model.m
+++ SkunkStats/CFB_build_model.m
@@ -1,76 +1,67 @@
-function model = CFB_build_model(lib, args)
+function model = CFB_build_model(lib, args, startYear, startWeek, endYear, endWeek)
 %
 addpath ../Base
 addpath C:\libsvm-3.18\matlab
 addpath C:\liblinear-1.94\matlab
 
-%% === Read in 2014-00 scores and stats ===
-fid = fopen('../Scores/Scores-2014-00.cfb', 'r');
-moreScores = true;
+%% === Loop through years and weeks ===
+iStartYear = eval(startYear);
+iStartWeek = eval(startWeek);
+iEndYear = eval(endYear);
+iEndWeek = eval(endWeek);
 trainData = zeros(0, 0);
 trainLabels = zeros(0, 1);
 iGame = 1;
-while moreScores
-    thisScore = fgetl(fid);
-    if thisScore == -1
-        moreScores = false;
-        continue
+for iYear = iStartYear:iEndYear
+    for iWeek = iStartWeek:iEndWeek
+%         if iWeek == 7
+%             disp('Skipping week 7')
+%             continue
+%         end
+        thisWeek = sprintf('0%d', iWeek);
+        thisWeek = thisWeek(end-1:end);
+        thisFile = sprintf('../Scores/Scores-%d-%s.cfb', iYear, thisWeek);
+        thisFid = fopen(thisFile, 'r');
+        moreScores = 1;
+        while moreScores
+            thisScore = fgetl(thisFid);
+            if thisScore == -1
+                moreScores = 0;
+                continue
+            end
+            % Lookup team index
+            commas = find(thisScore == ',');
+            team1 = thisScore(1:commas(1)-1);
+            team2 = thisScore(commas(1)+1:commas(2)-1);
+            iTeam1 = CFB_lookup(team1);
+            iTeam2 = CFB_lookup(team2);
+            % Get data features
+            sYear = sprintf('%d', iYear);
+            sWeek = sprintf('%d', iWeek);
+            trainData(iGame,:) = CFB_find_features(iTeam1, iTeam2, sYear, sWeek);
+            % Get labels
+            score1 = thisScore(commas(2)+1:commas(3)-1);
+            score2 = thisScore(commas(3)+1:end);
+            try
+                score1 = eval(score1);
+                score2 = eval(score2);
+            catch
+                disp('Warning: N/A scores, skipping')
+                continue
+            end
+            if score2 > score1
+                trainLabels(iGame,1) = 1;
+            else
+                trainLabels(iGame,1) = -1;
+            end
+            % Inc
+            iGame = iGame + 1;
+        end
+        fclose(thisFid);
     end
-    % Lookup team index
-    commas = find(thisScore == ',');
-    team1 = thisScore(1:commas(1)-1);
-    team2 = thisScore(commas(1)+1:commas(2)-1);
-    iTeam1 = CFB_lookup(team1);
-    iTeam2 = CFB_lookup(team2);
-    % Get data features
-    trainData(iGame,:) = CFB_find_features(iTeam1, iTeam2, '2014', '00');
-    % Get labels
-    score1 = thisScore(commas(2)+1:commas(3)-1);
-    score2 = thisScore(commas(3)+1:end);
-    score1 = eval(score1);
-    score2 = eval(score2);
-    if score2 > score1
-        trainLabels(iGame,1) = 1;
-    else
-        trainLabels(iGame,1) = -1;
-    end
-    % Inc
-    iGame = iGame + 1;
 end
 
-%% === Read in 2013-12 scores ===
-fclose(fid);
-fid = fopen('../Scores/Scores-2013-12.cfb', 'r');
-moreScores = true;
-while moreScores
-    thisScore = fgetl(fid);
-    if thisScore == -1
-        moreScores = false;
-        continue
-    end
-    % Lookup team index
-    commas = find(thisScore == ',');
-    team1 = thisScore(1:commas(1)-1);
-    team2 = thisScore(commas(1)+1:commas(2)-1);
-    iTeam1 = CFB_lookup(team1);
-    iTeam2 = CFB_lookup(team2);
-    % Get data features
-    trainData(iGame,:) = CFB_find_features(iTeam1, iTeam2, '2014', '00');
-    % Get labels
-    score1 = thisScore(commas(2)+1:commas(3)-1);
-    score2 = thisScore(commas(3)+1:end);
-    score1 = eval(score1);
-    score2 = eval(score2);
-    if score2 > score1
-        trainLabels(iGame,1) = 1;
-    else
-        trainLabels(iGame,1) = -1;
-    end
-    % Inc
-    iGame = iGame + 1;
-end
-
-%% === Train model, weights ===
+%% === Train the model ===
 trainData(trainData ~= trainData) = 0;
 if strcmp(lib, 'libsvm')
     model = svmtrain(trainLabels, trainData, args);
